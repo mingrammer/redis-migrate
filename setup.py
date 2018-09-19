566bece8405de02fb1ae98d952db3aa0abba7200
mingrammer <mingrammer@gmail.com>
2018-09-20 01:04:02 +0900

    Add abstract methods

--- setup.py
+++ setup.py
@@ -41,6 +41,12 @@ class PublishCommand(Command):
         """Prints things in bold."""
         print('\033[1m{0}\033[0m'.format(s))
 
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
     def run(self):
         try:
             self.status('Removing previous builds…')
